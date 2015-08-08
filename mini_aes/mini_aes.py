0529bbe41bcb318b631883ed46dbb6a6bdb1b517
Wei En <wei2912.supp0rt@gmail.com>
2015-08-08 15:45:58 +0800

    mini_aes/mini_aes.py: Overhaul and documentation of code.

--- mini_aes/mini_aes.py
+++ mini_aes/mini_aes.py
@@ -1,5 +1,3 @@
-# constants
-
 ROUNDS = 5
 
 SBOX = [
@@ -27,19 +25,96 @@ RCONS = [0x01, 0x02, 0x04, 0x08, 0x03]
 # procedures
 
 def nibble_sub(ns):
+    """
+    ns -> List of nibbles
+    
+    Passes each nibble through a S-Box. The mapping is as follows (in binary):
+
+    0000 -> 1110
+    0001 -> 0100
+    0010 -> 1101
+    0011 -> 0001
+    0100 -> 0010
+    0101 -> 1111
+    0110 -> 1011
+    0111 -> 1000
+    1000 -> 0011
+    1001 -> 1010
+    1010 -> 0110
+    1011 -> 1100
+    1100 -> 0101
+    1101 -> 1001
+    1110 -> 0000
+    1111 -> 0111
+    """
     return [SBOX[n] for n in ns]
 
 def inv_nibble_sub(ns):
+    """
+    ns -> List of nibbles
+
+    Retrieves the preimage of the mapping of each nibble. Refer to
+    `nibble_sub()` for more details.
+    """
     return [INV_SBOX[n] for n in ns]
 
 def shift_row(ns):
+    """
+    ns -> List of nibbles
+
+    Shift each row of the matrix of nibbles to the left by different amounts.
+
+    For example, given the list of nibbles, `[0, 1, 2, 3]`, they will be
+    arranged in a matrix layout,
+
+        0  2
+
+        1  3
+
+    from top to down, left to right.
+
+    The first row is unchanged while the second row is rotated left by one
+    nibble. This gives the resulting matrix:
+
+        0  2
+
+        3  1
+
+    or, in the form of a list, `[0, 3, 2, 1]`.
+    """
     assert len(ns) == 4
     tmp = ns[1]
     ns[1] = ns[3]
     ns[3] = tmp
-    return ns 
+    return ns
 
 def mix_column(ns):
+    """
+    ns -> List of nibbles
+
+    Multiplies the matrix of nibbles by a constant matrix.
+
+    For example, given the list of nibbles, `[0, 1, 2, 3]`, and arranging them
+    in a matrix format:
+
+        0  2
+
+        1  3
+
+    the matrix of nibbles will be multiplied by this constant matrix:
+
+        3  2
+
+        2  3
+
+    to form:
+
+        2  0
+
+        3  1
+
+    Note that multiplication is done in the finite field GF(2^4).
+    """
     assert len(ns) == 4
     n0 = ns[0]
     n1 = ns[1]
@@ -53,61 +128,127 @@ def mix_column(ns):
     ]
 
 def key_addition(ws, ns):
+    """
+    ws -> List of round key nibbles
+    ns -> List of nibbles
+
+    XORs each nibble with its corresponding round key nibble.
+    """
     assert len(ws) == len(ns) == 4
     return [w ^ n for (w, n) in zip(ws, ns)]
 
-# key schedule
-
 def key_schedule(k):
+    """
+    k -> Key
+
+    Generates round keys from a 16-bit key.
+
+    The first round key is derived directly from the supplied key.
+    """
     assert len(k) == 4
-    ws = [[]] * (ROUNDS + 1)
-    ws[0] = k
+    wss = [[]] * (ROUNDS + 1)
+    wss[0] = k
     for i in range(0, ROUNDS):
-        w = [0] * 4
-        w[0] = ws[i][0] ^ SBOX[ws[i][3]] ^ RCONS[i]
-        w[1] = ws[i][1] ^ w[0]
-        w[2] = ws[i][2] ^ w[1]
-        w[3] = ws[i][3] ^ w[2]
-        ws[i + 1] = w
-    return ws
+        ws = [0] * 4
+        ws[0] = wss[i][0] ^ SBOX[wss[i][3]] ^ RCONS[i]
+        ws[1] = wss[i][1] ^ ws[0]
+        ws[2] = wss[i][2] ^ ws[1]
+        ws[3] = wss[i][3] ^ ws[2]
+        wss[i + 1] = ws
+    return wss
 
 # encryption & decryption
 
-def round(ws, ns, i):
-    if i == 0:
-        ns = key_addition(ws[0], ns)
-    ns = shift_row(nibble_sub(ns))
+def roundf(ws, ns, i):
+    """
+    ws -> List of key nibbles
+    ns -> List of nibbles
+    i -> Round number
+
+    Round function of Mini-AES.
+
+    This function operates by performing the following operations in order:
+    1. KeyAddition (with ith round key)
+    2. NibbleSub
+    3. ShiftRow
+    4. MixColumn (if not the last round)
+
+    Note that this function does not handle the addition of the final round key.
+    """
+    assert len(ns) == 4
+    ns = shift_row(
+        nibble_sub(
+            key_addition(ws, ns)
+        )
+    )
     if i != ROUNDS - 1:
         ns = mix_column(ns)
-    ns = key_addition(ws[i + 1], ns)
     return ns
 
-def inv_round(ws, ns, i):
-    ns = key_addition(ws[i + 1], ns)
+def inv_roundf(ws, ns, i):
+    """
+    ws -> List of key nibbles
+    ns -> List of nibbles
+    i -> Round number
+
+    Inverse round function of Mini-AES. Refer to `roundf()` for more details.
+    """
+    assert len(ns) == 4
     if i != ROUNDS - 1:
         ns = mix_column(ns)
-    ns = inv_nibble_sub(shift_row(ns))
-    if i == 0:
-        ns = key_addition(ws[0], ns)
+    ns = key_addition(ws,
+        inv_nibble_sub(
+            shift_row(ns)
+        )
+    )
     return ns
 
 def encrypt_block(k, p):
+    """
+    k -> Key
+    p -> Plaintext
+
+    Encrypts the plaintext with Mini-AES, given the key.
+    """
     assert len(k) == len(p) == 4
-    ws = key_schedule(k)
-    assert len(ws) == ROUNDS + 1
+    wss = key_schedule(k)
+    assert len(wss) == ROUNDS + 1
 
     ns = p
     for i in range(0, ROUNDS):
-        ns = round(ws, ns, i)
+        ns = roundf(wss[i], ns, i)
+    ns = key_addition(wss[ROUNDS], ns)
     return ns
 
 def decrypt_block(k, c):
+    """
+    k -> Key
+    c -> Ciphertext
+    Decrypts the ciphertext encrypted with Mini-AES, given the key.
+    """
     assert len(k) == len(c) == 4
-    ws = key_schedule(k)
-    assert len(ws) == ROUNDS + 1
+    wss = key_schedule(k)
+    assert len(wss) == ROUNDS + 1
 
-    ns = c
+    ns = key_addition(wss[ROUNDS], c)
     for i in range(ROUNDS - 1, -1, -1):
-        ns = inv_round(ws, ns, i)
+        ns = inv_roundf(wss[i], ns, i)
     return ns
+
+# main function
+
+def main():
+    import random
+    k = [random.randint(0, 15) for _ in range(4)]
+    p = [random.randint(0, 15) for _ in range(4)]
+    print("Key: {}".format(k))
+    print("Plaintext: {}".format(p))
+
+    c = encrypt_block(k, p)
+    print("Ciphertext: {}".format(c))
+    d = decrypt_block(k, c)
+    print("Decrypted ciphertext: {}".format(d))
+
+if __name__ == "__main__":
+    main()
 
