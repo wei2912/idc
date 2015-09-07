962a7ecf97714084eb0c10d5c9283452660a7f25
Wei En <wei2912.supp0rt@gmail.com>
2015-09-07 23:44:53 +0800

    some_cipher/find_ids.py: Return list in more useful form (sorted by probability)

--- some_cipher/find_ids.py
+++ some_cipher/find_ids.py
@@ -25,32 +25,33 @@ def find_forward_diff(g, start):
         nss = list(gen_graph.chunks(ns, 4))
         return (sum(nss[0]), sum(nss[1]), sum(nss[2]))
 
-    cycles = set(nx.find_cycle(g))
+    def get_paths():
+        cycles = set(nx.find_cycle(g))
 
-    ps = [([start], 0)]
-    rounds = 0
-    isEnd = False
-    while len(ps) > 0 and not isEnd:
-        n_ps = []
-        for p, weight in ps:
-            v0 = p[-1]
-            for v1 in g[v0]:
-                if (v0, v1) in cycles:
-                    continue
-                if v1 == 4095:
-                    isEnd = True
+        ps = [([start], 0)]
+        rounds = 0
+        isEnd = False
+        while len(ps) > 0 and not isEnd:
+            n_ps = []
+            for p, weight in ps:
+                v0 = p[-1]
+                for v1 in g[v0]:
+                    if (v0, v1) in cycles or v1 == 4095:
+                        isEnd = True
 
-                v1_weight = g[v0][v1]["weight"]
-                n_ps.append((p + [v1], weight + v1_weight))
-        rounds += 1
-        ps = n_ps
+                    v1_weight = g[v0][v1]["weight"]
+                    n_ps.append((p + [v1], weight + v1_weight))
+            rounds += 1
+            ps = n_ps
+
+        return (rounds, ps)
 
     end_states = {}
     for p, w0 in ps:
         end_state = count_active(p[-1])
         if end_state in end_states:
             w1 = end_states[end_state]
-            end_states[end_state] = math.log(math.exp(w0) + math.exp(w1))
+            end_states[end_state] = -math.log(math.exp(-w0) + math.exp(-w1))
         else:
             end_states[end_state] = w0
     return (rounds, end_states)
@@ -62,9 +63,13 @@ def main():
         print("Start: {}".format(gen_graph.convert_int(start)))
         rounds, end_states = find_forward_diff(g, start)
         print("No. of rounds: {}".format(rounds))
-        for end_state, weight in end_states.items():
-            print("End state: {}".format(end_state))
-            print("Probability: {}".format(math.exp(-weight)))
+        ls = list(end_states.items())
+        ls.sort(key=lambda t: t[1])
+        for end_state, weight in ls:
+            print("{} ({})".format(
+                end_state,
+                math.exp(-weight)
+            ))
         print("---")
 
 if __name__ == "__main__":
