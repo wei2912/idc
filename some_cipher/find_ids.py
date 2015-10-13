b93d96cc979296c4d05f9daafd9bfa88307c3e18
Wei En <wei2912.supp0rt@gmail.com>
2015-10-13 21:37:46 +0800

    find_ids.py: Use a neater solution to find the forward and backward differentials.

--- some_cipher/find_ids.py
+++ some_cipher/find_ids.py
@@ -19,26 +19,24 @@ def find_diff(g, start):
     """
     cycles = set(nx.find_cycle(g))
 
-    ps = [[start]]
+    vs = set([start])
+    states = [vs]
     rounds = 0
+
     isEnd = False
-    while len(ps) > 0 and not isEnd:
-        n_ps = []
-        for p in ps:
-            v0 = p[-1]
+    while len(vs) > 0 and not isEnd:
+        n_vs = set()
+        for v0 in vs:
             for v1 in g[v0]:
                 if (v0, v1) in cycles or v1 == 4095:
                     isEnd = True
-                n_ps.append(p + [v1])
-        ps = n_ps
+                n_vs.add(v1)
+
+        vs = n_vs
+        states.append(vs)
         rounds += 1
 
-    second_last_states = set()
-    last_states = set()
-    for p in ps:
-        second_last_states.add(p[-2])
-        last_states.add(p[-1])
-    return (rounds, second_last_states, last_states)
+    return (rounds, states)
 
 def main():
     forward_g = nx.read_gpickle("forward.gpickle")
@@ -52,22 +50,18 @@ def main():
 
     ids = []
     for i in range(4096):
+        forward_rounds, xss = forward_diffs[i]
         for j in range(4096):
-            forward_rounds, xs0, xs1 = forward_diffs[i]
-            backward_rounds, ys0, ys1 = backward_diffs[j]
+            backward_rounds, yss = backward_diffs[j]
 
-            # truncating last round of forward differential
-            if xs0.intersection(ys1) == set():
-                forward_rounds -= 1
-            # truncating first round of backward differential
-            elif xs1.intersection(ys0) == set():
+            # truncate first round of backward differential
+            # by comparing last round of forward differential and second last
+            # round of backward differential
+            if xss[-1].intersection(yss[-2]) == set():
                 backward_rounds -= 1
-            else: # not an impossible differential
-                continue
-
-            if forward_rounds + backward_rounds < 3:
-                continue
-            ids.append((i, forward_rounds, backward_rounds, j))
+                if forward_rounds + backward_rounds < 3:
+                    continue
+                ids.append((i, forward_rounds, backward_rounds, j))
     print("Found {} impossible differentials.".format(len(ids)))
     with open("ids.pickle", "wb") as f:
         pickle.dump(ids, f)
