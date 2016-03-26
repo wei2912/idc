9f02005fc0cf9926271d9b20a6dc9bc466aeadff
Ng Wei En <weien1292@gmail.com>
2016-03-26 19:15:22 +0800

    Shift everything back into some_cipher folder, and restore the mini_aes folder.

---
+++ mini_aes/test_mini_aes.py
@@ -0,0 +1,49 @@
+"""
+Test suite for the `mini_aes` module.
+"""
+
+import pytest
+from pytest import list_of
+
+import mini_aes
+
+@pytest.mark.randomize(
+    ns=list_of(int),
+    min_num=0,
+    max_num=15
+)
+def test_nibble_sub(ns):
+    # Ensure that any list of nibbles, when passed through `nibble_sub` and
+    # `inv_nibble_sub`, returns the original list.
+    assert ns == mini_aes.inv_nibble_sub(mini_aes.nibble_sub(ns))
+
+@pytest.mark.randomize(
+    n0=int,
+    n1=int,
+    n2=int,
+    n3=int,
+    min_num=0,
+    max_num=15
+)
+def test_shift_row(n0, n1, n2, n3):
+    # Ensure that `shift_row` interchanges the second and fourth nibble.
+    assert mini_aes.shift_row([n0, n1, n2, n3]) == [n0, n3, n2, n1]
+
+@pytest.mark.randomize(
+    ns=list_of(int, items=4),
+    min_num=0,
+    max_num=15
+)
+def test_mix_column(ns):
+    # Ensure that `mix_column` is its own inverse.
+    assert ns == mini_aes.mix_column(mini_aes.mix_column(ns))
+
+@pytest.mark.randomize(
+    k=list_of(int, items=4),
+    p=list_of(int, items=4),
+    min_num=0,
+    max_num=15
+)
+def test_encrypt_decrypt(k, p):
+    # Make sure that decrypting an encrypted message returns the same plaintext.
+    assert p == mini_aes.decrypt_block(k, mini_aes.encrypt_block(k, p))
