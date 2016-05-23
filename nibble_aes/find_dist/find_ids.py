ed5a7d4ec4a9bc822ed4a8c691bcc6872490ade8
Ng Wei En <weien1292@gmail.com>
2016-05-23 23:00:00 +0800

    Have find_ids.py load from forward and backward differentials.

--- nibble_aes/find_dist/find_ids.py
+++ nibble_aes/find_dist/find_ids.py
@@ -3,7 +3,11 @@ Derive a list of impossible differentials.
 """
 
 def main():
-    # TODO: Read file format output.
+    with open("forward_diffs.pickle") as f:
+        forward_diffs = pickle.load(f)
+    with open("backward_diffs.pickle") as f:
+        backward_diffs = pickle.load(f)
+
     for i in range(65536):
         forward_rounds, xss = forward_diffs[i]
         for j in range(65536):
