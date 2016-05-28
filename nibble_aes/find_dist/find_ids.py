d19e4a358f1f81f72a02c3015fc4a0def2827e19
Ng Wei En <weien1292@gmail.com>
2016-05-28 22:11:15 +0800

    Optimize by using isdisjoint instead of finding intersection.

--- nibble_aes/find_dist/find_ids.py
+++ nibble_aes/find_dist/find_ids.py
@@ -18,23 +18,19 @@ def main():
     ids = []
     with open(sys.argv[1]) as f:
         for i, forward_rounds, xss in map(parse, f): 
+            if forward_rounds < 2:
+                continue
+
             with open(sys.argv[2]) as g:
                 for j, backward_rounds, yss in map(parse, g):
+                    if backward_rounds < 2:
+                        continue
+
                     # truncate first round of backward differential
                     # by comparing last round of forward differential and second last
                     # round of backward differential
-                    if xss[-1].intersection(yss[-2]) == set():
+                    if xss[-1].isdisjoint(yss[-2]):
                         backward_rounds -= 1
-                        rounds = forward_rounds + backward_rounds
-                    # or vice versa
-                    elif xss[-2].intersection(yss[-1]) == set():
-                        forward_rounds -= 1
-                        rounds = forward_rounds + backward_rounds
-                    # if there is no contradiction, skip
-                    else:
-                        continue
-
-                    if rounds >= 3:
                         print((i, forward_rounds, backward_rounds, j))
 
 if __name__ == "__main__":
