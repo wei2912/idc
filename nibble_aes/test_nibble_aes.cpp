fb221459663c3c45001a7942b22ee7fd8974675c
Ng Wei En <weien1292@gmail.com>
2016-06-11 10:24:03 +0800

    remove debugging flags

--- nibble_aes/test_nibble_aes.cpp
+++ nibble_aes/test_nibble_aes.cpp
@@ -10,7 +10,7 @@ int main() {
     std::default_random_engine g(rd());
     std::uniform_int_distribution<> d(0, 65535);
 
-    for (int i = 0; i < 10000000; ++i) {
+    for (int i = 0; i < 100000000; ++i) {
         uint16_t ks[4];
         uint16_t ps[4];
         for (int i = 0; i < 4; ++i) ks[i] = d(g);
