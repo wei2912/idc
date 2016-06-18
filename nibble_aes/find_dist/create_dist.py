c576db1dc35fa86403cb348827a7ee0dd47c3773
Ng Wei En <weien1292@gmail.com>
2016-06-18 22:21:13 +0800

    group distinguishers by start and end state

--- nibble_aes/find_dist/create_dist.py
+++ nibble_aes/find_dist/create_dist.py
@@ -2,6 +2,31 @@ from ast import literal_eval
 import math
 import sys
 
+def last_round(end):
+    bs = [end >> (15 - i) & 0x1 for i in range(16)]
+
+    tmp = bs[1]
+    bs[1] = bs[5]
+    bs[5] = bs[9]
+    bs[9] = bs[13]
+    bs[13] = tmp
+
+    tmp = bs[2]
+    bs[2] = bs[10]
+    bs[10] = tmp
+
+    tmp = bs[6]
+    bs[6] = bs[14]
+    bs[14] = tmp
+
+    tmp = bs[3]
+    bs[3] = bs[15]
+    bs[15] = bs[11]
+    bs[11] = bs[7]
+    bs[7] = tmp
+
+    return int("".join(str(b) for b in bs), 2)
+
 def main():
     if not len(sys.argv) == 5:
         print("usage: ./create_dist.py \"(s, t)\" [differentials file] [forward extensions file] [backward extensions file]", file=sys.stderr)
@@ -19,7 +44,7 @@ def main():
         for end, p, w in map(literal_eval, f):
             backward_exts.setdefault(end, []).append((p[1], w))
 
-    dists = []
+    dists = {}
     min_w = float('inf')
     with open(sys.argv[2]) as f:
         for start, end in map(literal_eval, f):
@@ -37,15 +62,20 @@ def main():
                         continue
 
                     if wf + wb < min_w:
-                        dists = []
+                        dists = {}
                         min_w = wf + wb
                     elif wf + wb > min_w:
                         continue
 
-                    dists.append((n_start, start, end, n_end, wf + wb))
+                    key = (n_start, last_round(n_end))
+                    val = (n_start, start, end, n_end, last_round(n_end), wf + wb)
+                    dists.setdefault(key, []).append(val)
 
-    for dist in dists:
-        print(dist)
+    for key in sorted(dists):
+        print("{}: {}".format(key, len(dists[key])))
+        for dist in dists[key]:
+            print(dist)
+        print()
 
 if __name__ == "__main__":
     main()
